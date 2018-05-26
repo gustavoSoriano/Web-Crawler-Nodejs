d2f80ffae7b40a925abfab871fabaf53a1a675ca
Gustavo Soriano <sorianox2010@hotmail.com>
2018-05-26 02:24:49 -0300

    simplificando a demo

--- index.js
+++ index.js
@@ -8,17 +8,13 @@ server.get('search/:busca', function(req, response){
 	request('https://www.google.com.br/search?q='+req.params.busca, function(err, res, body){
 		if(err) return console.log( err )
 
-		var $ = cheerio.load(body)
-		let title, link, description, results=[]
+		var $      = cheerio.load(body)
+		let results= []
 
 		$('.g').each(function(){
-			title       = $(this).find('.r a').text().trim()
-			link        = $(this).find('.s .kv cite').text().trim()
-			description = $(this).find('.s .st').text().trim()
-			if(title && link && description) results.push( {titulo:title, url:link, descricao:description} )
+			results.push( $(this).html() )
 		})
-
-		response.json( results )
+		response.end( results.join("") )
 	})
 	
 })
